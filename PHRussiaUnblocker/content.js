5923538c1067f5cf687a03e06a144305bec61972
Paketov <somedser@outlook.com>
2022-10-19 05:29:07 +0000

    Adding files

---
+++ PHRussiaUnblocker/content.js
@@ -0,0 +1,32 @@
+﻿(function () {
+    console.log("PHUnblocker: content.js loaded");
+    function insertPageScript(text) {
+        let parent = document.documentElement;
+        let script = document.createElement('script');
+        script.text = text;
+        script.async = false;
+        parent.insertBefore(script, parent.firstChild);
+        parent.removeChild(script);
+    }
+    function insertPageScriptEx(textOrFunc, vals) {
+        if (typeof (textOrFunc) == "function")
+            textOrFunc = "(" + textOrFunc + (")(" + JSON.stringify(vals) + ");");
+        insertPageScript(textOrFunc);
+    }
+
+setTimeout(function (){
+        var injectFunc = function injectFunc(param) {
+
+            console.log("PHUnblocker: script enjected for " + self.location);
+            ///age-verification-container
+                        //age-verification-wrapper
+            document.getElementById("age-verification-container").outerHTML = "";
+            document.getElementById("age-verification-wrapper").outerHTML = "";
+            console.log("PHUnblocker: enjected script executed");
+        };
+        insertPageScriptEx(injectFunc, {});
+}, 500);
+        
+
+})();
+
---
+++ readme.txt
@@ -0,0 +1,4 @@
+~~~~~~~~~~~~~~~~~~PHRussiaUnblocker~~~~~~~~~~~~~~~~~~
+
+For pornhub.com site. In Russia not show pop-up with 
+age confirmation. 
